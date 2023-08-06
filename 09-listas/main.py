725df6c3fd3a395468d981485c188892616d328b
mcdalenas <root@Mcs-MacBook-Pro.local>
2023-08-06 15:26:21 +0200

    0508

---
+++ 09-listas/main.py
@@ -0,0 +1,20 @@
+"""
+LISTAS / ARRAY
+COLECCIONES DE DATOS BAJO UN UNICO NOMBRE
+PODEMOS USAR UN INDICE NUMERICO PARA ACCEDER A ELLOS.
+"""
+
+pelicula = "batman"
+print(pelicula)
+
+peliculas = ["Batman", "Spiderman", "El señor de los anillos"]
+print(peliculas)
+
+cantantes = list(("Drake", "Bbno", "Manolo"))
+print(cantantes)
+
+years = list(range(2020,2050))
+print(years)
+
+variada = ["joel", 22, 1.75, False, "Texto"]
+print(variada)
